6d00f514e7f5ab665bd20262f535b84af91afd03
Logan Nelson <Lnelson0915@gmail.com>
2025-07-25 10:30:44 -0700

    Add Product clas
    Introduces a new `Product` class representing individual products for sale.
    Includes properties for `ProductId`, `Title`, and `Price`, with appropriate
    data annontations for validation, such as `[Key]`, `[StringLength]`, and
    `[Range]` to enforce constraints on the data.

---
+++ eCommerce/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models;
+
+/// <summary>
+/// Represents an individual product for sale
+/// </summary>
+public class Product
+{   
+    /// <summary>
+    /// The unique identifier for the product
+    /// </summary>
+    [Key]
+    public int ProductID { get; set; }
+
+    /// <summary>
+    /// The user facing title of the product
+    /// </summary>
+    [StringLength(50, ErrorMessage = "Titles cannot be more than 50 characters")]
+    public required string Title { get; set; }
+
+    /// <summary>
+    /// The current sales price of the product
+    /// </summary>
+    [Range(0, 10000)]
+    public double Price { get; set; }
+}

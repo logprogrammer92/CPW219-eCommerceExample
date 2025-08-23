3a5f8c13cbf13d26e0db69984119a79a4f446c25
Logan Nelson <Lnelson0915@gmail.com>
2025-08-23 14:31:00 -0700

    Finished basic login functionality to MemberController and view
    Updated MemberController to include login logic with validation against the database. Added POST method to handle user credentials and redirect on success. Modified Login.cshtml to use POST method for form submission.

--- eCommerce/Controllers/MemberController.cs
+++ eCommerce/Controllers/MemberController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerce.Data;
 using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Controllers
 {
@@ -44,6 +45,30 @@ namespace eCommerce.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if UsernameOrEmail and Password match a record in the database
+                Member? loggedInMember = await _context.Members
+                                     .Where(m => (m.Username == login.UsernameOrEmail || m.Email == login.UsernameOrEmail)
+                                         && m.Password == login.Password)
+                                         .SingleOrDefaultAsync();
+
+                if (loggedInMember == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Your provided credentials do not match any records in our database");
+                    return View(login);
+                }
+
+                    // Log the user in???
+
+                    return RedirectToAction("Index", "Home");
+            }
+            return View(login);
         }
     }
 }

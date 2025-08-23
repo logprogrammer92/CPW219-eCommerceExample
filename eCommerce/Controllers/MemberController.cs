6ae1c1174b1596fbe4e0214527a071aa9d52f662
Logan Nelson <Lnelson0915@gmail.com>
2025-08-23 12:21:35 -0700

    Update MemberController for registration handling
    Added ProductDbContext dependency and constructro to MemberController. Refactored Register method to handle asynchronous registration with RegistrationViewModel, including validation and member creation.

--- eCommerce/Controllers/MemberController.cs
+++ eCommerce/Controllers/MemberController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
 {
     public class MemberController : Controller
     {
+        private readonly ProductDbContext _context;
+
+        public MemberController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map ViewModel to Member model tracked by DB
+                Member newMember = new()
+                {
+                    Username = reg.Username,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    DateOfBirth = reg.DateOfBirth
+                };
+
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(reg);
         }
     }
 }
